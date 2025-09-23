527f52efa37772648cebd0994b881ef88c39127d
Matt18-M <robert.mateo.3012@gmail.com>
2025-09-23 15:38:14 -0500

    se muestra texto e imagenes

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -6,8 +6,24 @@ saludar=function(){
 
     let edad=recuperarInt("txtEdad");
     let estatura=recuperarFloat("txtEstatura");
+
+    let mensajeBienvenida="Bienvenido " + nombre + " " + apellido;
+    mostrarTexto("lblResultado", mensajeBienvenida);
+    
+    mostrarImagen("imgSaludo","./imagenes/saludo.gif");
 }
 
+mostrarTexto = function(idComponente,mensaje){
+let componente;
+componente=document.getElementById(idComponente);
+componente.innerText = mensaje;
+}
+
+mostrarImagen = function(idComponente, rutaImagen){
+let componente;
+componente=document.getElementById(idComponente);
+componente.src = rutaImagen;
+}
 
 recuperarTexto = function(idComponente){
 let componente;
