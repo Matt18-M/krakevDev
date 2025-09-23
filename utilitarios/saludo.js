6f78429f3d343b02b8d7dcb5ec9471eef6216552
Matt18-M <robert.mateo.3012@gmail.com>
2025-09-23 15:47:45 -0500

    se separan las funciones al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -8,49 +8,8 @@ saludar=function(){
     let estatura=recuperarFloat("txtEstatura");
 
     let mensajeBienvenida="Bienvenido " + nombre + " " + apellido;
-    mostrarTexto("lblResultado", mensajeBienvenida);
-    
+    mostrarTexto("lblResultado", mensajeBienvenida);    
     mostrarImagen("imgSaludo","./imagenes/saludo.gif");
     mostrarTextoEnCaja("txtNombre", "");
 
-}
-
-mostrarTexto = function(idComponente,mensaje){
-let componente;
-componente=document.getElementById(idComponente);
-componente.innerText = mensaje;
-}
-
-
-mostrarImagen = function(idComponente, rutaImagen){
-let componente;
-componente=document.getElementById(idComponente);
-componente.src = rutaImagen;
-}
-
-mostrarTextoEnCaja = function(idComponente,mensaje){
-let componente;
-componente=document.getElementById(idComponente);
-componente.value = mensaje;
-}
-
-recuperarTexto = function(idComponente){
-let componente;
-let valorIngresado;
-componente = document.getElementById(idComponente);
-valorIngresado = componente.value;
-return valorIngresado;
-
-}
-
-recuperarInt = function (idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat = function (idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorFloat = parseFloat(valorCaja);
-    return valorFloat;
 }
