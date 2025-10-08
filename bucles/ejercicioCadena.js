5ec48a11c6376b89fb926182239feefc87eb24eb
Matt18-M <robert.mateo.3012@gmail.com>
2025-10-08 13:33:07 -0500

    se corrigio errores

--- bucles/ejercicioCadena.js
+++ bucles/ejercicioCadena.js
@@ -4,30 +4,32 @@ ejecutarPrueba1 = function(){
     recorrerCadena(mensaje);
 }
 
-
 recorrerCadena = function(cadena){
     let caracter;
 
-    for(let posicion = 0; posicion < cadena.length; posicion++){
+    // Validar que la cadena no esté vacía
+    if(!cadena || cadena.length === 0){
+        console.log("La cadena está vacía");
+        return;
+    }
+
+    for(let posicion=0;posicion<cadena.length;posicion++){
         caracter = cadena.charAt(posicion);
         console.log("Caracter " + caracter + " posicion " + posicion);
     }
 
-    for(let posicion = 0; posicion <= cadena.length - 1; posicion++){
+    for(let posicion=0;posicion<= cadena.length-1;posicion++){
         caracter = cadena.charAt(posicion);
         console.log("CARACTER " + caracter + " posicion " + posicion);
     }
-
 }
 
-function invertirCadena(cadena) {
+function invertirCadena() {
+    let cadena = recuperarTexto("txtInvertir");
     let resultado = "";
     
     for(let letra = cadena.length - 1; letra >= 0; letra--) {
         resultado += cadena.charAt(letra);
-        resultado = resultado + letra;
     }    
-mostrarTexto ("lblCadena" + resultado);  
+    cambiarTexto("lblInvertir", resultado);  
 }
-
-
