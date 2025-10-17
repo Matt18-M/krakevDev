6c896c863115545b0049c324e0f1ca31d8d75434
Matt18-M <robert.mateo.3012@gmail.com>
2025-10-16 19:39:50 -0500

    se creo la funcion mostrarEmpleados y se dio diseño ala tabla html

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,36 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0150560811",nombre:"Mateo",apellido:"Molina",sueldo:630.0}
 ]
+
+mostrarEmpleados = function(){
+        let cmpTabla = document.getElementById("tablaEmpleados");
+        let conetenidoTabla = "<table class='tabla-estilo'>" + "<tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+        let elementoEmpleado;
+        for (let i = 0; i < empleados.length; i++){
+                elementoEmpleado=empleados[i];
+                conetenidoTabla +=
+                "<tr><td>" + elementoEmpleado.cedula + "</td>"
+                + "<td>" + elementoEmpleado.nombre + "</td>"
+                + "<td>" + elementoEmpleado.apellido + "</td>"
+                + "<td>" + elementoEmpleado.sueldo + "</td>"
+                +"</tr>"
+        }
+        conetenidoTabla += "</table>"
+        cmpTabla.innerHTML=conetenidoTabla;
+    }
 
 function mostrarOpcionEmpleado() {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 function mostrarOpcionRol() {
